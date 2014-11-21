e603652d18642b7ed19ec69e73f39525f5d0cd56
eremid <r.v.m@free.fr>
2014-11-21 14:25:16 +0100

    ADD: progress bar/status on mcollective actions

--- public/js/kmb.mco.show.js
+++ public/js/kmb.mco.show.js
@@ -11,7 +11,7 @@ function addInputField(object,argname,details)
 
 function addSelectBox(object,argname, details)
 {
-	object.append('<div class="form-group"><label class="control-label" for="'+ argname +'">'+ details['prompt'] +'</label><div class="controls row"><div class="col-lg-12"><select id="'+ argname +'list_arg_chosen" data-placeholder="---" class="form-control" name="'+ argname +'" data-rel="chosen"> <option value="default"></option></select></div></div></div>');
+    object.append('<div class="form-group"><label class="control-label" for="'+ argname +'">'+ details['prompt'] +'</label><div class="controls row"><div class="col-lg-12"><select id="'+ argname +'list_arg_chosen" data-placeholder="---" class="form-control" name="'+ argname +'" data-rel="chosen"> <option value="default"></option></select></div></div></div>');
     $.each(details['list'], function(item,value){
 	$("#"+argname+'list_arg_chosen').append(
 	    $('<option></option>').val(value).html(value)
@@ -20,8 +20,67 @@ function addSelectBox(object,argname, details)
     $("#"+argname+'list_arg_chosen').chosen();
 }
 
+function getResult(data,target,discovered_nodes,refreshResult)
+{
+    $ .ajax({
+        type: 'GET',
+	url: data['resultUrl'] + '?state=finished',
+	dataType: 'json',
+	success: function(data,status) {
+            resultsReceived = Object.keys(data).length;
+	    console.log("Success in resultUrl : " + resultsReceived);
+            NProgress.set(resultsReceived/discovered_nodes);
+            // console.log('polling... Received : ' + resultsReceived + ' - Discovered : ' + discovered_nodes);
+
+            $legend.html('Ex&eacute;cution de la requ&ecirc;te <span class="label label-success">OK</span><br/>');
+
+            if (resultsReceived == discovered_nodes) {
+                $legend.append('<strong>Réception des données : done</strong><br/>');
+                // console.log('Stopping polling... Received : ' + resultsReceived + ' - Discovered : ' + discovered_nodes);
+                clearInterval(refreshResult);
+                NProgress.done();
+            }
+            else
+            {
+                $legend.append('<strong>Réception des données : ' + resultsReceived + ' sur ' + discovered_nodes + ' serveurs.</strong><br/>');
+            }
+
+            target.html('');
+
+	    $.each(data, function(hostname,result){
+		$.each(result, function(index,jsondata){
+		    obj = jsondata;
+                    if (obj['statuscode'] != null) {
+				    if(obj['statuscode'] != 0)
+			{
+			    var tag = '<span class="label label-danger" id="serveurs_security">Error : '+ obj['result']+'</span>';
+                            $legend.append('<a href="#' + hostname + '">' + hostname + '</a> <span class="label label-danger">KO</span><br/>');
+			}else{
+			    var tag = '<span class="label label-success" id="serveurs_security">OK</span>';
+                            $legend.append('<a href="#' + hostname + '">' + hostname + '</a> <span class="label label-success">OK</span><br/>');
+			}
+			var tablist = '<div class="panel-body"><ul class="nav tab-menu nav-tabs" data-tabs="tabs">';
+			var divresult = '<div class="tab-content">';
+			$.each(JSON.parse(obj['result']),function(name,value){
+			    tablist += '<li><a class="output" href="#mcol_'+ hostname.replace(/\./g,'_') +'_'+ name +'" data-toggle="tab">'+name+'</a></li>';
+			    divresult += '<div class="tab-pane" id="mcol_'+ hostname.replace(/\./g,'_') +'_'+ name +'"><pre>'+ value+'</pre></div>';
+			});
+			tablist += '</ul>';
+			divresult += '</div>';
+			target.append('<div class="panel panel-default"><div class="panel-heading"><h3 id=' + hostname + '><i class="glyphicon glyphicon-remove"></i>&nbsp;'+ hostname +'&nbsp;'+tag+'</div>'+tablist+divresult);
+			target.find("li .output").first().addClass('active');
+			target.find(".tab-pane").first().addClass('active');
+                    }
+		});
+	    });
+	}
+    });
+}
+
 $(document).ready(function(){
     var agents = null;
+    $legend = $("#legende_mcol");
+
     $.ajax({
 	'async': false,
 	'global': false,
@@ -34,7 +93,7 @@ $(document).ready(function(){
 
     $.each(agents, function(key, value) {
 	$("#selectAgent").append(
-	    $('<option></option>').val(key).html(key) 
+	    $('<option></option>').val(key).html(key)
 	);
     });
 
@@ -44,10 +103,10 @@ $(document).ready(function(){
 	$('#selectAction option[value!="default"]').remove();
 	$.each(agents[agent], function(key,value) {
 	    $("#selectAction").append(
-		$('<option></option>').val(key).html(key + ' - ' + value['summary']) 
+		$('<option></option>').val(key).html(key + ' - ' + value['summary'])
 	    ).trigger('chosen:updated');
 	});
-    }); 
+    });
 
     $(document).on('change', '.selectAction', function() {
 	$(".arg_mcol").html('');
@@ -65,16 +124,15 @@ $(document).ready(function(){
 	    }
 	});
 	$("#form_arg_mcol").append('<div class="form-group"><input id="arglist" type="hidden" name="args" value="'+ arglist +'"></input></div>');
-    }); 
+    });
+
     $(document).on('submit', 'form[data-async]',function(event) {
+        NProgress.start();
+        $legend.css('font-size','10.5px');
 	var $form = $(this);
 	var $target = $($form.attr('data-target'));
 	$target.html('');
-	$("#legend").html(' \
-<h2>Ex&eacute;cution de la requ&ecirc;te en cours</h2> \
-\
-<img alt="waiting.." id="wait_img" src="/images/wait.gif"></img>\
-');
+        $legend.html('Ex&eacute;cution de la requ&ecirc;te <span class="label label-primary">UP</span>');
 	$.ajax({
 	    type: $form.attr('method'),
 	    url: $form.attr('action'),
@@ -82,50 +140,26 @@ $(document).ready(function(){
 	    dataType: 'json',
 	    success: function(data, status) {
 		console.log("Success in run");
-		$.ajax({
-		    type: 'GET',
-		    url: data['resultUrl'],
-		    dataType: 'json',
-		    success: function(data,status) {
-			console.log("Success in resultUrl");
-			$.each(data, function(hostname,result){
-			    $.each(result, function(index,jsondata){
-				//				obj = JSON.parse(jsondata);
-				obj = jsondata;
-				if(obj['statuscode'] != 0)
-				{
-				    var tag = '<span class="label label-danger" id="serveurs_security">Error : '+ obj['result']+'</span>';
-				}else{
-				    var tag = '<span class="label label-success" id="serveurs_security">OK</span>';
-				}
-				var tablist = '<div class="panel-body"><ul class="nav tab-menu nav-tabs" data-tabs="tabs">';
-				var divresult = '<div class="tab-content">';
-				$.each(JSON.parse(obj['result']),function(name,value){
-				    tablist += '<li><a class="output" href="#mcol_'+ hostname.replace(/\./g,'_') +'_'+ name +'" data-toggle="tab">'+name+'</a></li>';
-				    divresult += '<div class="tab-pane" id="mcol_'+ hostname.replace(/\./g,'_') +'_'+ name +'"><pre>'+ value+'</pre></div>';
-				});
-				tablist += '</ul>';
-				divresult += '</div>';
-				$target.append('<div class="panel panel-default"><div class="panel-heading"><h3><i class="glyphicon glyphicon-remove"></i>&nbsp;'+ hostname +'&nbsp;'+tag+'</div>'+tablist+divresult);
-				$target.find("li .output").first().addClass('active');
-				$target.find(".tab-pane").first().addClass('active');
-			    });
-			});
-		    }
-		});
-		$("#action_mcol :input").prop("disabled", false);
-		$("#legend").html('');
+                $legend.html('Ex&eacute;cution de la requ&ecirc;te <span class="label label-success">OK</span><br/>');
+                var discovered_nodes = data['discovered_nodes'].length;
+
+                var refreshResult = setInterval(function() {
+                                        getResult(data,$target,discovered_nodes,refreshResult);
+                                    }, 10000);
+
+                getResult(data,$target,discovered_nodes,refreshResult);
+
+                $("#action_mcol :input").prop("disabled", false);
 	    },
 	    error: function(data,status) {
 		$("#legend").html('<ul class="flash"><li class="alert alert-danger"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button><b> '+ data['statusText'] +'</b></li></ul>');
 		$("#wait_img").remove();
 		$("#action_mcol :input").prop("disabled", false);
-	    }	    
+                NProgress.done();
+	    }
 	});
-	$("#action_mcol :input").prop("disabled", true);	
+
+	$("#action_mcol :input").prop("disabled", true);
 	event.preventDefault();
     });
 });
-
-
-
